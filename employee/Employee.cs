981c54c161aeff20a29449e362fe7e774d888746
KayqueS <kayque.sanmartin65@gmail.com>
2021-03-02 20:06:47 -0300

    Add project files.

---
+++ employee/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace employee
+{
+    class Employee
+    {
+        public string Name;
+        public double GrossSalary;
+        public double Tax;
+
+        public double NetSalary()
+        {
+            return GrossSalary - Tax;
+        }
+
+        public void IncreaseSalary(double increaseSalary)
+        {
+            GrossSalary += (GrossSalary * increaseSalary / 100.0); 
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, $ {NetSalary():F2}";
+        }
+    }
+}
---
+++ employee/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee x = new Employee();
+
+            Console.Write($"Name: ");
+            x.Name = Console.ReadLine();
+            Console.Write($"Gross salary: ");
+            x.GrossSalary = double.Parse(Console.ReadLine());
+            Console.Write($"Tax: ");
+            x.Tax = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Employee: {x}");
+
+            Console.Write("Enter the percentage to increase the salary: ");
+            double increase = double.Parse(Console.ReadLine());
+            x.IncreaseSalary(increase);
+
+            Console.WriteLine($"Updated data: {x}");
+        }
+    }
+}
